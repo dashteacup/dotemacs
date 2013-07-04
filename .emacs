c3aec979cf550d1142210d305f0108be9266462c
Paul Curry <dashteacup@gmail.com>
2013-07-04 14:29:20 -0500

    Renamed custom lisp file directories.
    The latest verions of GNU emacs have made .emacs.d/ the default
    directory for user lisp customizations.  I converted my .elisp/
    directory to the new standard for consistancy.  I also converted my
    somewhat confusingly named site-lisp/ directory to a packages/
    directory to distinguish it from the actual site-lisp/ directory used
    by emacs for system wide lisp packages.  This change required updating
    both documentation and paths in elisp files.

--- .emacs
+++ .emacs
@@ -1,6 +1,6 @@
 ;;;; Paul's .emacs file
 ;; Originally written sometime before September 7, 2003
-;; Time-stamp: <2013-07-03 18:48:03 pcurry>
+;; Time-stamp: <2013-07-04 14:19:54 pcurry>
 
 ;;; Note: This should be in the user's home directory (~).
 ;;; Or you could just make ~/.emacs a symbolic link to this file.
@@ -22,17 +22,17 @@
  )
 
 ;;; My lisp files directories
-(add-to-list 'load-path "~/.elisp/")
+(add-to-list 'load-path "~/.emacs.d/")
 
 ;;; My user installed packages.
-(add-to-list 'load-path "~/.elisp/site-lisp")
+(add-to-list 'load-path "~/.emacs.d/packages")
 
 ;;; Autoloads of user installed packages.
 (condition-case nil
-    (load-file "~/.elisp/site-lisp/loaddefs.el")
-  (error (load-file "~/.elisp/site-lisp/update-autoloads.el")
+    (load-file "~/.emacs.d/packages/loaddefs.el")
+  (error (load-file "~/.emacs.d/packages/update-autoloads.el")
          (update-autoloads-for-packages)
-         (load-file "~/.elisp/site-lisp/loaddefs.el")))
+         (load-file "~/.emacs.d/packages/loaddefs.el")))
 
 ;;; Load all my custom lisp files.
 (mapc 'load-library
--- packages/update-autoloads.el
+++ packages/update-autoloads.el
@@ -1,15 +1,15 @@
 ;; Author: Paul Curry
 ;; Created: 2013-07-03
-;; Time-stamp: <2013-07-03 19:03:07 pcurry>
+;; Time-stamp: <2013-07-04 14:22:14 pcurry>
 
 ;;; Description: Make magic autoloads work for user installed .el files.
 
 ;;; Installation:
 ;; Place this file in your user installed package directory. For me it's
-;; ~/.elisp/site-lisp. Run the `update-autoloads-for-packages' function to
+;; ~/.emacs.d/packages. Run the `update-autoloads-for-packages' function to
 ;; generate a loaddefs.el in your package directory. Then, load the
 ;; loaddefs.el from you .emacs file.  I use
-;; (load-file "~/.elisp/site-lisp/loaddefs.el")
+;; (load-file "~/.emacs.d/packages/loaddefs.el")
 
 ;; Modified from: http://stackoverflow.com/questions/4189159/emacs23-elisp-how-to-properly-autoload-this-library
 
@@ -18,13 +18,13 @@
   "Update loaddefs.el with the magic autoloads found in my package directory.
 Creates a new loaddefs.el using the magic autoloads (;;;###autoload)
 found in the files in you package directory. The loaddefs.el is
-located in the same directory. "
+located in the same directory."
   (interactive)
   (let ((package-directory (file-truename
                             (file-name-directory
                              (symbol-file 'update-autoloads-for-packages 'defun)))))
     (require 'autoload)
     ;; Use dynamic binding to change location of loaddefs
-    ;; (generate-autoload-file) used by update-file-autoloads
+    ;; (the generate-autoload-file variable) used by update-file-autoloads
     (let ((generated-autoload-file (concat package-directory "loaddefs.el")))
       (update-directory-autoloads package-directory))))
