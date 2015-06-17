07c6b552097cb10f1a014798a4106bd60ac8082c
Paul Curry <dashteacup@gmail.com>
2015-06-17 13:46:59 -0500

    Move .el to my-lisp/ dir.
    Emacs gives me warnings about putting my .el files in ~/.emacs.d/
    Apparantly some programs will write files to that dir, so emacs
    recommended that I make a subdirectory for my .el files to prevent
    name collisions.

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 ;;;; Paul's init.el file
 ;; Originally written sometime before September 7, 2003
-;; Time-stamp: <2013-07-14 22:43:09 pcurry>
+;; Time-stamp: <2015-06-17 13:44:59 currypx>
 
 ;;; This used to be my .emacs file, but Emacs 22 and up allows you to
 ;;; use ~/.emacs.d/init.el as your config file, so it got a name change.
 
 ;; These paths are constants.
-(let* ((elisp-dir "~/.emacs.d/")
-       (package-dir (concat elisp-dir "packages/"))
+(let* ((elisp-dir "~/.emacs.d/my-lisp/")
+       (package-dir "~/.emacs.d/packages/")
        (loaddefs (concat package-dir "loaddefs.el"))
        (autoloader (concat package-dir "update-autoloads.el")))
 
@@ -18,10 +18,11 @@
   (add-to-list 'load-path package-dir)
 
   ;; Autoloads of user installed packages.
-  (unless (file-exists-p loaddefs)
-    (load-file autoloader)
-    (update-autoloads-for-packages))
-  (load-file loaddefs))
+  (when (featurep 'aquamacs)
+    (unless (file-exists-p loaddefs)
+      (load-file autoloader)
+      (update-autoloads-for-packages))
+    (load-file loaddefs)))
 
 ;;; Load all my custom lisp files.
 (mapc 'load-library
