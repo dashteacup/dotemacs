6f9ac6caff72abdda1c62f99ad58ede1c313838f
Paul Curry <dashteacup@gmail.com>
2018-07-18 12:41:40 -0500

    Add custom section to init.el with installed packages.

--- init.el
+++ init.el
@@ -1,9 +1,23 @@
 ;;;; Paul's init.el file
 ;; Originally written sometime before September 7, 2003
-;; Time-stamp: <2015-07-17 10:24:21 currypx>
+;; Time-stamp: <2018-07-18 12:35:03 pcurry>
 
 ;;; This used to be my .emacs file, but Emacs 22 and up allows you to
 ;;; use ~/.emacs.d/init.el as your config file, so it got a name change.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ido-vertical-mode))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; These paths are constants.
 (let* ((elisp-dir "~/.emacs.d/my-lisp/")
