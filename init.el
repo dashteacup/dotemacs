00c53a2ae0afff12dbabb4d055cafb96f8727078
Paul Curry <dashteacup@gmail.com>
2018-07-18 20:14:26 -0500

    Add more packages to package-selected-packages.

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;;; Paul's init.el file
 ;; Originally written sometime before September 7, 2003
-;; Time-stamp: <2018-07-18 12:35:03 pcurry>
+;; Time-stamp: <2018-07-18 20:14:09 pcurry>
 
 ;;; This used to be my .emacs file, but Emacs 22 and up allows you to
 ;;; use ~/.emacs.d/init.el as your config file, so it got a name change.
@@ -10,7 +10,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ido-vertical-mode))))
+ '(package-selected-packages
+   (quote
+    (evil buffer-move csv-mode ws-trim ido-vertical-mode))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
