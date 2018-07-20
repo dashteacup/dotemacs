2554302652468b037af5ecd9e05ad77631fb2f16
Paul Curry <dashteacup@gmail.com>
2018-07-20 16:59:31 -0500

    Replace lambda hooks with named functions.
    Named functions can be disabled with remove-hook, which can be helpful
    when debugging.

--- my-lisp/modes.el
+++ my-lisp/modes.el
@@ -1,6 +1,6 @@
 ;; Author: Paul Curry
 ;; Created: 2006-10-27
-;; Time-stamp: <2018-01-18 17:39:52 currypx>
+;; Time-stamp: <2018-07-20 16:19:50 pcurry>
 
 ;;; Description: Configuration for many different modes.
 ;; Note that hooks can only contain function names not function calls.
@@ -28,143 +28,161 @@
 (add-to-list 'magic-mode-alist '("#!/bin/sh" . sh-mode))
 (add-to-list 'magic-mode-alist '("#!/.*python" . python-mode))
 
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (c-set-style "stroustrup")
-            (setq c-tab-always-indent nil)
-            (c-toggle-hungry-state t)
-            (setq c-block-comment-prefix "")
-            ; Use C++ style comments, which C has supported since c99.
-            (setq comment-start "//")
-            (setq comment-end "")
-            (if (srequire 'subword) (subword-mode 1))
-            (if (srequire 'flymake) (flymake-mode 1))
-            (when (srequire 'flyspell) (flyspell-prog-mode))
-            (local-set-key "\C-cc" 'compile)
-            (local-set-key "\C-j" 'comment-indent-new-line)
-            (local-set-key [f7] 'make-unit-tests)))
-
-(add-hook 'makefile-mode-hook
-          (lambda ()
-            (local-set-key "\C-cc" 'compile)))
-
-(add-hook 'java-mode-hook
-          (lambda ()
-            (when (srequire 'flyspell) (flyspell-prog-mode))
-            (c-set-style "java")
-            (c-toggle-hungry-state t)))
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (if (srequire 'subword) (subword-mode 1))
-            (when (srequire 'flyspell) (flyspell-prog-mode))
-            (when (and (srequire 'flymake)
-                       (exec-in-path-p "pylint"))
-              (flymake-mode 1)
-              (define-key python-mode-map "\C-cn" 'flymake-goto-next-error)
-              (define-key python-mode-map "\C-cp" 'flymake-goto-prev-error))))
-
-(add-hook 'cperl-mode-hook
-          (lambda ()
-            (cperl-set-style "PerlStyle")
-            ;; Hide the stupid trailing whitespace underline.
-            (setq cperl-invalid-face nil)
-            (local-set-key "\C-hp" 'cperl-perldoc)
-            (if (srequire 'flymake) (flymake-mode 1))))
-
 (setq maude-command "/usr/local/maude-intelDarwin/maude")
 
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (when (srequire 'flyspell) (flyspell-prog-mode))
-            ;; for emacs source code
-            (setq tab-width 8)
-            (eldoc-mode 1)))
+(defun my-c-mode-common-hook ()
+  (c-set-style "stroustrup")
+  (setq c-tab-always-indent nil)
+  (c-toggle-hungry-state t)
+  (setq c-block-comment-prefix "")
+  ; Use C++ style comments, which C has supported since c99.
+  (setq comment-start "//")
+  (setq comment-end "")
+  (if (srequire 'subword) (subword-mode 1))
+  (if (srequire 'flymake) (flymake-mode 1))
+  (when (srequire 'flyspell) (flyspell-prog-mode))
+  (local-set-key "\C-cc" 'compile)
+  (local-set-key "\C-j" 'comment-indent-new-line)
+  (local-set-key [f7] 'make-unit-tests))
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
-(add-hook 'tcl-mode-hook
-          (lambda ()
-            (when (srequire 'flyspell) (flyspell-prog-mode))))
 
-(add-hook 'sh-mode-hook
-          (lambda ()
-            (when (srequire 'flyspell) (flyspell-prog-mode))))
+(defun my-makefile-mode-hook ()
+  (local-set-key "\C-cc" 'compile))
+(add-hook 'makefile-mode-hook 'my-makefile-mode-hook)
 
+
+(defun my-java-mode-hook ()
+  (when (srequire 'flyspell) (flyspell-prog-mode))
+  (c-set-style "java")
+  (c-toggle-hungry-state t))
+(add-hook 'java-mode-hook 'my-java-mode-hook)
+
+
+(defun my-python-mode-hook ()
+  (if (srequire 'subword) (subword-mode 1))
+  (when (srequire 'flyspell) (flyspell-prog-mode))
+  (when (and (srequire 'flymake)
+             (exec-in-path-p "pylint"))
+    (flymake-mode 1)
+    (define-key python-mode-map "\C-cn" 'flymake-goto-next-error)
+    (define-key python-mode-map "\C-cp" 'flymake-goto-prev-error)))
+(add-hook 'python-mode-hook 'my-python-mode-hook)
+
+
+(defun my-cperl-mode-hook ()
+  (cperl-set-style "PerlStyle")
+  ;; Hide the stupid trailing whitespace underline.
+  (setq cperl-invalid-face nil)
+  (local-set-key "\C-hp" 'cperl-perldoc)
+  (if (srequire 'flymake) (flymake-mode 1)))
+(add-hook 'cperl-mode-hook 'my-cperl-mode-hook)
+
+
+(defun my-emacs-lisp-mode-hook ()
+  (when (srequire 'flyspell) (flyspell-prog-mode))
+  ;; for emacs source code
+  (setq tab-width 8)
+  (eldoc-mode 1))
+(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
+
+
+(defun my-tcl-mode-hook ()
+  (when (srequire 'flyspell) (flyspell-prog-mode)))
+(add-hook 'tcl-mode-hook 'my-tcl-mode-hook)
+
+
+(defun my-sh-mode-hook ()
+  (when (srequire 'flyspell) (flyspell-prog-mode)))
+(add-hook 'sh-mode-hook 'my-sh-mode-hook)
+
+
+(defun my-text-mode-hook ()
+  (when (featurep 'aquamacs)
+    (smart-spacing-mode 1)
+    ;; Need to update my console version of emacs so I can use this.
+    (visual-line-mode 1))
+  (when (srequire 'flyspell)
+    ;; Need to set up ispell before I can use this in a terminal.
+    (flyspell-mode 1)))
 ;; default doesn't work well for me
 (if (featurep 'aquamacs) (setq text-mode-hook nil))
-(add-hook 'text-mode-hook
-          (lambda ()
-            (when (featurep 'aquamacs)
-              (smart-spacing-mode 1)
-              ;; Need to update my console version of emacs so I can use this.
-              (visual-line-mode 1))
-            (when (srequire 'flyspell)
-              ;; Need to set up ispell before I can use this in a terminal.
-              (flyspell-mode 1))))
+(add-hook 'text-mode-hook 'my-text-mode-hook)
+
 
 ;; auctex
-(add-hook 'LaTeX-mode-hook
-          (lambda ()
-            (setq TeX-PDF-mode t)
-            (setq TeX-output-view-style
-                  (cons '("^pdf$" "." "open %o") TeX-output-view-style))
-            (local-set-key "\C-cc"
-                           (lambda ()
-                             (interactive)
-                             (save-buffer)
-                             (TeX-command TeX-command-default
-                                          'TeX-master-file)))
-            (add-to-list 'font-latex-quote-list '("``" "\""))
-            (setq skeleton-pair t)
-            (mapc (lambda (char)
-                    (local-set-key char 'skeleton-pair-insert-maybe))
-                  '("(" "[" "{"))))
+(defun my-LaTeX-mode-hook ()
+  (setq TeX-PDF-mode t)
+  (setq TeX-output-view-style
+        (cons '("^pdf$" "." "open %o") TeX-output-view-style))
+  (local-set-key "\C-cc"
+                 (lambda ()
+                   (interactive)
+                   (save-buffer)
+                   (TeX-command TeX-command-default
+                                'TeX-master-file)))
+  (add-to-list 'font-latex-quote-list '("``" "\""))
+  (setq skeleton-pair t)
+  (mapc (lambda (char)
+          (local-set-key char 'skeleton-pair-insert-maybe))
+        '("(" "[" "{")))
+(add-hook 'LaTeX-mode-hook 'my-LaTeX-mode-hook)
+
 
 (defun my-org-mode-hook ()
   (setq evil-auto-indent nil))
 (add-hook 'org-mode-hook 'my-org-mode-hook)
 
-(add-hook 'asm-mode-hook
-          (lambda ()
-            (setq asm-comment-char 35)
-            (local-set-key (kbd "<tab>") 'indent-relative)
-            (local-set-key (kbd "<return>") 'newline)
-            (local-set-key "#" 'self-insert-command)
-            (local-set-key ":" 'self-insert-command)))
 
-(add-hook 'write-file-hooks
-          (lambda ()
-            (unless (featurep 'ws-trim)
-              (delete-trailing-whitespace))
-            (time-stamp)
-            (copyright-update)))
+(defun my-asm-mode-hook ()
+  (setq asm-comment-char 35)
+  (local-set-key (kbd "<tab>") 'indent-relative)
+  (local-set-key (kbd "<return>") 'newline)
+  (local-set-key "#" 'self-insert-command)
+  (local-set-key ":" 'self-insert-command))
+(add-hook 'asm-mode-hook 'my-asm-mode-hook)
+
+
+(defun my-write-file-hooks ()
+  (unless (featurep 'ws-trim)
+    (delete-trailing-whitespace))
+  (time-stamp)
+  (copyright-update))
+(add-hook 'write-file-hooks 'my-write-file-hooks)
+
 
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
 
+
 ;;; Copied from: http://www.emacswiki.org/emacs/EmacsClient#toc36
 ;; Use C-x k to properly kill an emacsclient.
-(add-hook 'server-switch-hook
-          (lambda ()
-            (when (current-local-map)
-              (use-local-map (copy-keymap (current-local-map))))
-            (when server-buffer-clients
-              (local-set-key (kbd "C-x k") 'server-edit))))
+(defun my-server-switch-hook ()
+  (when (current-local-map)
+    (use-local-map (copy-keymap (current-local-map))))
+  (when server-buffer-clients
+    (local-set-key (kbd "C-x k") 'server-edit)))
 ;;; End Copy
+(add-hook 'server-switch-hook 'my-server-switch-hook)
 
-(add-hook 'help-mode-hook
-          (lambda ()
-            (define-key help-mode-map "l" 'help-go-back)))
 
-(add-hook 'dired-mode-hook
-          (lambda ()
-            (local-set-key "\C-cc" 'compile)))
+(defun my-help-mode-hook ()
+  (define-key help-mode-map "l" 'help-go-back))
+(add-hook 'help-mode-hook 'my-help-mode-hook)
 
-(add-hook 'flymake-mode-hook
-          (lambda ()
-            ;; I HATE dialog boxes.
-            (setq flymake-gui-warnings-enabled nil)
-            ;; wait these seconds before rerunning flymake
-            (setq-default flymake-no-changes-timeout 1.5)))
+
+(defun my-dired-mode-hook ()
+  (local-set-key "\C-cc" 'compile))
+(add-hook 'dired-mode-hook 'my-dired-mode-hook)
+
+
+(defun my-flymake-mode-hook ()
+  ;; I HATE dialog boxes.
+  (setq flymake-gui-warnings-enabled nil)
+  ;; wait these seconds before rerunning flymake
+  (setq-default flymake-no-changes-timeout 1.5))
+(add-hook 'flymake-mode-hook 'my-flymake-mode-hook)
+
 
 ;; flymake has no hook variable.  Annoying.
 ;; TODO: Yes it does.  Alter this code appropriately.
@@ -231,5 +249,4 @@
         (append flymake-allowed-file-name-masks flymake-allowed-js-file-name-masks))
   (setq flymake-err-line-patterns flymake-js-err-line-patterns)
   (flymake-mode t))
-
-(add-hook 'javascript-mode-hook '(lambda () (flymake-js-load)))
+(add-hook 'javascript-mode-hook 'flymake-js-load)
