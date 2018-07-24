7d53634c49d6af47f113b0900befc70eb0a63678
Paul Curry <dashteacup@gmail.com>
2018-07-24 14:15:21 -0500

    Set default font for mac.

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;;; Paul's init.el file
 ;; Originally written sometime before September 7, 2003
-;; Time-stamp: <2018-07-18 20:16:02 pcurry>
+;; Time-stamp: <2018-07-24 14:10:38 pcurry>
 
 ;;; This used to be my .emacs file, but Emacs 22 and up allows you to
 ;;; use ~/.emacs.d/init.el as your config file, so it got a name change.
@@ -19,7 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "nil" :family "Menlo")))))
 
 ;; Necessary to be able to require installed packages.
 (package-initialize)
